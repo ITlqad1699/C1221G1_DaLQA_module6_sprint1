ebc394cfda937a5c609b62d4a48cc61d287909ed
ITlqad1699 <lqad1649engineer@gmail.com>
2022-06-27 08:34:55 +0700

    DaLQA update FE templates 27/06

---
+++ sprint1_fe/sprint1/src/app/reports/statistital-chart/statistital-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistital-chart',
+  templateUrl: './statistital-chart.component.html',
+  styleUrls: ['./statistital-chart.component.css']
+})
+export class StatistitalChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
